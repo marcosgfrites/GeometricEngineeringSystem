59594be14052c8a287468ca5bd60a5886fa279b9
Mauro <maurociparelli@gmail.com>
2019-10-26 21:19:41 -0300

    PruebaMauroProgram

--- PrimerasHU-GES/PrimerasHU-GES/Program.cs
+++ PrimerasHU-GES/PrimerasHU-GES/Program.cs
@@ -16,7 +16,7 @@ namespace PrimerasHU_GES
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MuestraMarcos()); //
+            Application.Run(new Usuarios()); //
 
         }
     }
