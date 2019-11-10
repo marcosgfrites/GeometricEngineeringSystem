afa0daa6dbb97d9efd8713353a529932978db43f
Pa8blote <49599619+Pa8blote@users.noreply.github.com>
2019-11-09 22:31:56 -0300

    Indicadores de calidad
    Update

--- PrimerasHU-GES/PrimerasHU-GES/IndicadoresCalidad.Designer.cs
+++ PrimerasHU-GES/PrimerasHU-GES/IndicadoresCalidad.Designer.cs
@@ -38,16 +38,19 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.dtIndicadores = new System.Windows.Forms.DataGridView();
-            this.ges_v01DataSet18 = new PrimerasHU_GES.ges_v01DataSet18();
             this.indicadoresCalidadBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ges_v01DataSet18 = new PrimerasHU_GES.ges_v01DataSet18();
             this.indicadoresCalidadTableAdapter = new PrimerasHU_GES.ges_v01DataSet18TableAdapters.indicadoresCalidadTableAdapter();
             this.codIndicadorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.fechoraModIndicadorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.nomIndicadorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.valorIndicadorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dtIndicadores)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ges_v01DataSet18)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.indicadoresCalidadBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ges_v01DataSet18)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -71,9 +74,9 @@
             // 
             // txtValor
             // 
-            this.txtValor.Location = new System.Drawing.Point(35, 296);
+            this.txtValor.Location = new System.Drawing.Point(35, 302);
             this.txtValor.Name = "txtValor";
-            this.txtValor.Size = new System.Drawing.Size(200, 20);
+            this.txtValor.Size = new System.Drawing.Size(174, 20);
             this.txtValor.TabIndex = 2;
             // 
             // txtNomIndi
@@ -85,10 +88,12 @@
             // 
             // dtFecha
             // 
+            this.dtFecha.Format = System.Windows.Forms.DateTimePickerFormat.Short;
             this.dtFecha.Location = new System.Drawing.Point(35, 218);
             this.dtFecha.Name = "dtFecha";
             this.dtFecha.Size = new System.Drawing.Size(200, 20);
             this.dtFecha.TabIndex = 6;
+            this.dtFecha.Value = new System.DateTime(2019, 11, 9, 0, 0, 0, 0);
             // 
             // label2
             // 
@@ -112,7 +117,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(31, 61);
+            this.label4.Location = new System.Drawing.Point(31, 19);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(301, 24);
             this.label4.TabIndex = 9;
@@ -130,21 +135,22 @@
             this.nomIndicadorDataGridViewTextBoxColumn,
             this.valorIndicadorDataGridViewTextBoxColumn});
             this.dtIndicadores.DataSource = this.indicadoresCalidadBindingSource;
-            this.dtIndicadores.Location = new System.Drawing.Point(330, 142);
+            this.dtIndicadores.Location = new System.Drawing.Point(356, 39);
             this.dtIndicadores.Name = "dtIndicadores";
             this.dtIndicadores.ReadOnly = true;
-            this.dtIndicadores.Size = new System.Drawing.Size(422, 174);
+            this.dtIndicadores.Size = new System.Drawing.Size(422, 243);
             this.dtIndicadores.TabIndex = 10;
-            // 
-            // ges_v01DataSet18
-            // 
-            this.ges_v01DataSet18.DataSetName = "ges_v01DataSet18";
-            this.ges_v01DataSet18.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            this.dtIndicadores.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtIndicadores_CellClick);
             // 
             // indicadoresCalidadBindingSource
             // 
             this.indicadoresCalidadBindingSource.DataMember = "indicadoresCalidad";
             this.indicadoresCalidadBindingSource.DataSource = this.ges_v01DataSet18;
+            // 
+            // ges_v01DataSet18
+            // 
+            this.ges_v01DataSet18.DataSetName = "ges_v01DataSet18";
+            this.ges_v01DataSet18.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // indicadoresCalidadTableAdapter
             // 
@@ -153,36 +159,67 @@
             // codIndicadorDataGridViewTextBoxColumn
             // 
             this.codIndicadorDataGridViewTextBoxColumn.DataPropertyName = "codIndicador";
-            this.codIndicadorDataGridViewTextBoxColumn.HeaderText = "codIndicador";
+            this.codIndicadorDataGridViewTextBoxColumn.HeaderText = "Cod. Indicador";
             this.codIndicadorDataGridViewTextBoxColumn.Name = "codIndicadorDataGridViewTextBoxColumn";
             this.codIndicadorDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // fechoraModIndicadorDataGridViewTextBoxColumn
             // 
             this.fechoraModIndicadorDataGridViewTextBoxColumn.DataPropertyName = "fechoraModIndicador";
-            this.fechoraModIndicadorDataGridViewTextBoxColumn.HeaderText = "fechoraModIndicador";
+            this.fechoraModIndicadorDataGridViewTextBoxColumn.HeaderText = "Fecha ";
             this.fechoraModIndicadorDataGridViewTextBoxColumn.Name = "fechoraModIndicadorDataGridViewTextBoxColumn";
             this.fechoraModIndicadorDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // nomIndicadorDataGridViewTextBoxColumn
             // 
             this.nomIndicadorDataGridViewTextBoxColumn.DataPropertyName = "nomIndicador";
-            this.nomIndicadorDataGridViewTextBoxColumn.HeaderText = "nomIndicador";
+            this.nomIndicadorDataGridViewTextBoxColumn.HeaderText = "Nombre";
             this.nomIndicadorDataGridViewTextBoxColumn.Name = "nomIndicadorDataGridViewTextBoxColumn";
             this.nomIndicadorDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // valorIndicadorDataGridViewTextBoxColumn
             // 
             this.valorIndicadorDataGridViewTextBoxColumn.DataPropertyName = "valorIndicador";
-            this.valorIndicadorDataGridViewTextBoxColumn.HeaderText = "valorIndicador";
+            this.valorIndicadorDataGridViewTextBoxColumn.HeaderText = "Valor";
             this.valorIndicadorDataGridViewTextBoxColumn.Name = "valorIndicadorDataGridViewTextBoxColumn";
             this.valorIndicadorDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(240, 370);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(117, 34);
+            this.btnModificar.TabIndex = 11;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(418, 370);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(117, 34);
+            this.btnBorrar.TabIndex = 12;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(604, 370);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(117, 34);
+            this.btnSalir.TabIndex = 13;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
             // 
             // IndicadoresCalidad
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnModificar);
             this.Controls.Add(this.dtIndicadores);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -196,8 +233,8 @@
             this.Text = "IndicadoresCalidad";
             this.Load += new System.EventHandler(this.IndicadoresCalidad_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dtIndicadores)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ges_v01DataSet18)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.indicadoresCalidadBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ges_v01DataSet18)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -221,5 +258,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn fechoraModIndicadorDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn nomIndicadorDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn valorIndicadorDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnSalir;
     }
 }
--- PrimerasHU-GES/PrimerasHU-GES/IndicadoresCalidad.cs
+++ PrimerasHU-GES/PrimerasHU-GES/IndicadoresCalidad.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Globalization;
 
 namespace PrimerasHU_GES
 {
@@ -19,6 +20,7 @@ namespace PrimerasHU_GES
         }
         private SqlConnection conexion;
         private SqlDataAdapter adaptador;
+        int posicion = 0;
 
 
         private void IndicadoresCalidad_Load(object sender, EventArgs e)
@@ -44,9 +46,7 @@ namespace PrimerasHU_GES
         private void btnRegistrar_Click(object sender, EventArgs e)
         {
 
-            adaptador.InsertCommand.Parameters["@fechoraModIndicador"].Value = dtFecha.Value;
-            adaptador.InsertCommand.Parameters["@nomIndicador"].Value = txtNomIndi.Text;
-            adaptador.InsertCommand.Parameters["@valorIndicador"].Value = txtValor.Text;
+           
 
             if (string.IsNullOrEmpty(txtValor.Text) || string.IsNullOrEmpty(txtValor.Text))
 
@@ -58,10 +58,15 @@ namespace PrimerasHU_GES
                 try
                 {
                     conexion.Open();
-                   
+
+                    adaptador.InsertCommand.Parameters["@fechoraModIndicador"].Value = dtFecha.Value;
+                    adaptador.InsertCommand.Parameters["@nomIndicador"].Value = txtNomIndi.Text;
+                    adaptador.InsertCommand.Parameters["@valorIndicador"].Value = Decimal.Parse(txtValor.Text);
+
                     adaptador.InsertCommand.ExecuteNonQuery();
 
                     MessageBox.Show("Registro de indicador cargado con exito");
+
                 }
 
                 catch (Exception ex)
@@ -74,7 +79,89 @@ namespace PrimerasHU_GES
 
 
                 }
+                SqlDataAdapter da = new SqlDataAdapter("select * from indicadoresCalidad", conexion);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dtIndicadores.DataSource = dt;
+                LimpiarIndicador();
             }
+
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            SqlCommand modificar = new SqlCommand("UPDATE indicadoresCalidad SET  fechoraModIndicador=@fechoraModIndicador, nomIndicador=@nomIndicador , valorIndicador=@valorIndicador  WHERE nomIndicador=@nomIndicador", conexion);
+            adaptador.UpdateCommand = modificar;
+
+            
+            adaptador.UpdateCommand.Parameters.Add(new SqlParameter("@fechoraModIndicador", SqlDbType.Date));
+            adaptador.UpdateCommand.Parameters.Add(new SqlParameter("@nomIndicador", SqlDbType.VarChar));
+            adaptador.UpdateCommand.Parameters.Add(new SqlParameter("@valorIndicador", SqlDbType.Int));
+
+           
+            adaptador.UpdateCommand.Parameters["@fechoraModIndicador"].Value = dtFecha.Value.ToString("dd-MM-yyyy");
+            adaptador.UpdateCommand.Parameters["@nomIndicador"].Value = txtNomIndi.Text;
+            adaptador.UpdateCommand.Parameters["@valorIndicador"].Value = Decimal.Parse(txtValor.Text);
+
+            int busqueda = 0;
+            if (string.IsNullOrEmpty(txtNomIndi.Text) || string.IsNullOrEmpty(txtValor.Text) && busqueda == 0)
+            {
+                MessageBox.Show("Por seguridad de los datos, primero debe seleccionar el Indicador a modificar, recuerde que para ésto debe hacer 'Clic' sobre alguna celda del panel que represente al Indicador. Por cualquier duda o consulta comuniquese con un Administrador", "Atención!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                try
+                {
+                    conexion.Open();
+                   
+                    if (MessageBox.Show("¿Está seguro que desea modificar el Indicador seleccionado: " + txtNomIndi.Text + "?", "Atención!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        MessageBox.Show("El Indicador: " + txtNomIndi.Text + " - " + txtValor.Text + ", fue modificado correctamente.", "Operación Exitosa!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        LimpiarIndicador();
+                        adaptador.UpdateCommand.ExecuteNonQuery();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("ERROR:" + ex.ToString());
+                }
+                finally
+                {
+                    conexion.Close();
+                }
+                SqlDataAdapter da = new SqlDataAdapter("select * from indicadoresCalidad", conexion);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dtIndicadores.DataSource = dt;
+            }
+        }
+        private void dtIndicadores_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+            posicion = dtIndicadores.CurrentRow.Index;
+         
+            txtNomIndi.Text = dtIndicadores[2, posicion].Value.ToString();
+            txtValor.Text = dtIndicadores[3, posicion].Value.ToString();
+            DateTime t = new DateTime();
+
+            if (!DateTime.TryParse(dtIndicadores[1, posicion].Value.ToString(),out t))
+            {
+                t = Convert.ToDateTime(dtIndicadores[1, posicion].Value.ToString(), System.Globalization.CultureInfo.GetCultureInfo("en-Us").DateTimeFormat);
+            }
+
+            dtFecha.Value = t;
+
+        }
+
+        private void LimpiarIndicador()
+        {
+            txtNomIndi.Text = "";
+            txtValor.Text = "";
+
+            this.indicadoresCalidadTableAdapter.Fill(this.ges_v01DataSet18.indicadoresCalidad);
+           
+           
         }
     }
 }
